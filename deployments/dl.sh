5ec603efbc796085efa4c96aa55e537b30af9cf2
ookkoouu <29059223+ookkoouu@users.noreply.github.com>
2021-10-01 08:29:14 +0000

    fix run scripts

--- deployments/dl.sh
+++ deployments/dl.sh
@@ -1,9 +1,9 @@
 cd $(dirname $0)
-if [ -n $GH_TOKEN ]; then
+if [ -z $GH_TOKEN ]; then
   echo "GH_TOKEN is not set."
   exit
 fi
 curl -sLJO -H 'Accept: application/octet-stream' \
   "https://$GH_TOKEN@api.github.com/repos/ookkoouu/ExpandMediaBot/releases/assets/$(\
     curl -sL https://$GH_TOKEN@api.github.com/repos/ookkoouu/ExpandMediaBot/releases/latest \
-      |jq '.assets[] | select(.name | contains("app")) | .id')"
+      |jq '.assets[] | select(.name | contains("arm32")) | .id')"
