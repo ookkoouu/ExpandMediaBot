8ea4aab2b8b33c390399a307970d44a741bd5b4f
ookkoouu <29059223+ookkoouu@users.noreply.github.com>
2021-10-03 17:22:38 +0000

    twitter操作をtwiutilに分離

--- cmd/handler/twitter.go
+++ cmd/handler/twitter.go
@@ -3,15 +3,12 @@ package handler
 import (
 	"context"
 	"log"
-	"net/url"
-	"regexp"
-	"sort"
-	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/kelseyhightower/envconfig"
+	"github.com/ookkoouu/twiutil"
 	"golang.org/x/oauth2/clientcredentials"
 )
 
@@ -39,72 +36,26 @@ func initTwitter() {
 	twClient = twitter.NewClient(httpClient)
 }
 
-func getVideoUrl(media twitter.MediaEntity) (url string) {
-	variants := make([]twitter.VideoVariant, 1, 4)
-	for _, v := range media.VideoInfo.Variants {
-		if v.ContentType == "video/mp4" {
-			variants = append(variants, v)
-		}
-	}
-	sort.Slice(variants, func(i, j int) bool { return variants[i].Bitrate > variants[j].Bitrate })
-	url = variants[0].URL
-	return
-}
-
 func getTweet(id int64) (tweet *twitter.Tweet, err error) {
 	tweet, _, err = twClient.Statuses.Show(id, nil)
 	return
-}
-
-func getMediaUrls(id int64) (urls []string) {
-	tweet, err := getTweet(id)
-	if err != nil || len(tweet.ExtendedEntities.Media) == 0 {
-		return
-	}
-
-	medias := tweet.ExtendedEntities.Media
-	if medias[0].Type == "photo" {
-		urls = make([]string, 0, 4)
-		for _, e := range medias {
-			urls = append(urls, e.MediaURLHttps)
-		}
-		urls = urls[1:]
-	} else if medias[0].Type == "video" || medias[0].Type == "animated_gif" {
-		urls = append(urls, getVideoUrl(medias[0]))
-	}
-	return
-}
-
-func getIdFromUrl(twUrl string) (id int64) {
-	u, err := url.Parse(twUrl)
-	if err != nil {
-		return 0
-	}
-	idStr := u.Path[strings.LastIndex(u.Path, "/")+1:]
-	id, err = strconv.ParseInt(idStr, 10, 64)
-	if err != nil {
-		return 0
-	}
-	return
-}
-
-func getTwitterUrl(content string) (urls []string) {
-	regexTwitterUrl := regexp.MustCompile(`https?://twitter\.com(/\w+)?/status(es)?/\d+`)
-	return regexTwitterUrl.FindAllString(content, -1)
 }
 
 func ExpandTwitter(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.Bot {
 		return
 	}
-	
-	twUrls := getTwitterUrl(m.Content)
-	if len(twUrls) > 0 {
+
+	tweetIDs := twiutil.FindIdAll(m.Content)
+	if len(tweetIDs) > 0 {
 		log.Println("ExpandTwitter called")
-		for _, url := range twUrls {
-			id := getIdFromUrl(url)
-			medias := getMediaUrls(id)
-			s.ChannelMessageSend(m.ChannelID, strings.Join(medias, "\n"))
+		for _, id := range tweetIDs {
+			tweet, err := getTweet(id)
+			if err != nil {
+				return
+			}
+			medias := twiutil.GetMediaUrlsString(*tweet)
+			s.ChannelMessageSend(m.ChannelID, strings.Join(medias[1:], "\n"))
 
 			log.Println(id, medias)
 		}
--- go.mod
+++ go.mod
@@ -4,20 +4,21 @@ go 1.17
 
 require (
 	github.com/bwmarrin/discordgo v0.23.2
-	github.com/dghubble/go-twitter v0.0.0-20210609183100-2fdbf421508e
+	github.com/dghubble/go-twitter v0.0.0-20211002212826-ad02880e616b
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/ookkoouu/twiutil v1.1.0
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
 )
 
 require (
-	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
-	github.com/dghubble/sling v1.3.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
-	golang.org/x/sys v0.0.0-20211001092434-39dca1131b70 // indirect
+	golang.org/x/sys v0.0.0-20211003122950-b1ebd4e1001c // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
