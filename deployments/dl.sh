97c35e77fa1a47c6cbd2d9ced2202d86e042627b
ookkoouu <29059223+ookkoouu@users.noreply.github.com>
2021-11-01 11:52:20 +0000

    バイナリをDockerfile内で落とす

--- deployments/dl.sh
+++
@@ -1,9 +0,0 @@
-cd $(dirname $0)
-if [ -z $GH_TOKEN ]; then
-  echo "GH_TOKEN is not set."
-  exit
-fi
-curl -sLJO -H 'Accept: application/octet-stream' \
-  "https://$GH_TOKEN@api.github.com/repos/ookkoouu/ExpandMediaBot/releases/assets/$(\
-    curl -sL https://$GH_TOKEN@api.github.com/repos/ookkoouu/ExpandMediaBot/releases/latest \
-      |jq '.assets[] | select(.name=="app_arm") | .id')"
